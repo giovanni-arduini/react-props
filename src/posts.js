03d012de40a48c61995cb993896fd14f280a0b51
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-28 15:08:29 +0100

    ADD image path in post array

--- src/posts.js
+++ src/posts.js
@@ -3,40 +3,40 @@
  */
 
 export const posts = [
-	{
-		id: 1,
-		title: 'Titolo del Post',
-		image: '' /* compila questo campo */,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
-		tags: ['html', 'css'],
-		published: true,
-	},
-	{
-		id: 2,
-		title: 'Titolo del Post',
-		image: '' /* compila questo campo */,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
-		tags: ['js', 'css'],
-		published: true,
-	},
-	{
-		id: 3,
-		title: 'Titolo del Post',
-		image: '' /* compila questo campo */,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
-		tags: ['js', 'php'],
-		published: true,
-	},
-	{
-		id: 4,
-		title: 'Titolo del Post',
-		image: '' /* compila questo campo */,
-		content:
-			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.',
-		tags: ['html'],
-		published: false,
-	},
-]
+  {
+    id: 1,
+    title: "Titolo del Post",
+    image: "https://picsum.photos/200/300" /* compila questo campo */,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html", "css"],
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Titolo del Post",
+    image: "https://picsum.photos/200/300" /* compila questo campo */,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "css"],
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Titolo del Post",
+    image: "https://picsum.photos/200/300" /* compila questo campo */,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["js", "php"],
+    published: true,
+  },
+  {
+    id: 4,
+    title: "Titolo del Post",
+    image: "https://picsum.photos/200/300" /* compila questo campo */,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit animi unde quasi enim non esse ratione voluptas voluptate, officiis veritatis magni blanditiis possimus nobis cum id inventore corporis deserunt hic.",
+    tags: ["html"],
+    published: false,
+  },
+];

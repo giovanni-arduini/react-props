4fddcb5f709ab2c86ea54c922e573f71322fbd99
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-28 17:56:50 +0100

    FIX function getClassByTag

--- src/components/posts/Card.jsx
+++ src/components/posts/Card.jsx
@@ -1,8 +1,29 @@
 import Button from "../UI/button/button";
 import style from "./Card.module.css";
 
+function getClassByTag(tag) {
+  let className;
+  switch (tag) {
+    case "js":
+      return (className = style.js);
+    case "php":
+      return (className = style.php);
+    case "html":
+      return (className = style.html);
+    case "css":
+      return (className = style.css);
+
+    default:
+      className = "";
+      break;
+  }
+
+  return className;
+}
+
 export default function card(props) {
   const tags = props.tags;
+
   return (
     <div className={style.card}>
       <img className={style.thumbnail} src={props.image} alt="" />
@@ -10,29 +31,14 @@ export default function card(props) {
       <div className={style.body}>
         <h3 className={style.title}>{props.title}</h3>
         <div key={props.id}>
-          {tags.map(
-            (tag) =>
-              (tag === "js" && (
-                <span key={tag} className={`${style.js} ${style.cardTag}`}>
-                  {tag}
-                </span>
-              )) ||
-              (tag === "php" && (
-                <span key={tag} className={`${style.php} ${style.cardTag}`}>
-                  {tag}
-                </span>
-              )) ||
-              (tag === "css" && (
-                <span key={tag} className={`${style.css} ${style.cardTag}`}>
-                  {tag}
-                </span>
-              )) ||
-              (tag === "html" && (
-                <span key={tag} className={`${style.html} ${style.cardTag}`}>
-                  {tag}
-                </span>
-              ))
-          )}
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className={`${getClassByTag(tag)} ${style.cardTag}`}
+            >
+              {tag}
+            </span>
+          ))}
         </div>
         <div className={style.description}>{props.description}</div>
         <div>

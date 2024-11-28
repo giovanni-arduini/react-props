e8106c5e242fad2345e869ea56e05f4a478849c5
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-28 17:03:59 +0100

    ADD tags colors in Card

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,5 @@
+import style from "./Tags.module.css";
+
+<span key={tag} className={props.tag((style = { php, cardTag }))}>
+  {tag}
+</span>;
--- src/components/posts/Card.jsx
+++ src/components/posts/Card.jsx
@@ -1,11 +1,8 @@
-import image from "../../assets/images.png";
 import Button from "../UI/button/button";
 import style from "./Card.module.css";
-// import { posts } from "../../posts";
 
 export default function card(props) {
   const tags = props.tags;
-
   return (
     <div className={style.card}>
       <img className={style.thumbnail} src={props.image} alt="" />
@@ -13,11 +10,29 @@ export default function card(props) {
       <div className={style.body}>
         <h3 className={style.title}>{props.title}</h3>
         <div key={props.id}>
-          {tags.map((tag) => (
-            <span key={tag} className={style.cardTag}>
-              {tag}
-            </span>
-          ))}
+          {tags.map(
+            (tag) =>
+              (tag === "js" && (
+                <span key={tag} className={`${style.js} ${style.cardTag}`}>
+                  {tag}
+                </span>
+              )) ||
+              (tag === "php" && (
+                <span key={tag} className={`${style.php} ${style.cardTag}`}>
+                  {tag}
+                </span>
+              )) ||
+              (tag === "css" && (
+                <span key={tag} className={`${style.css} ${style.cardTag}`}>
+                  {tag}
+                </span>
+              )) ||
+              (tag === "html" && (
+                <span key={tag} className={`${style.html} ${style.cardTag}`}>
+                  {tag}
+                </span>
+              ))
+          )}
         </div>
         <div className={style.description}>{props.description}</div>
         <div>

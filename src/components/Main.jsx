43a9affe0ae97a77c3569743a1cb4b518ff49158
giovanni-arduini <gionnibigudd@gmail.com>
2024-11-28 15:39:39 +0100

    FIX footer & col-6 style

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,7 +6,7 @@ export default function Main() {
       <section>
         <div className="container">
           <div className="row">
-            <div className="col-12">
+            <div className="col-6">
               <Card />
             </div>
           </div>
